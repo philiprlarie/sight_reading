973c8ade6bdcfe3a5ae5ac8de13ee8922c0e9d9b
philiprlarie <philiprlarie@gmail.com>
2015-11-13 23:38:31 -0800

    add metronome

--- javascripts/play_melody_sound.js
+++ javascripts/play_melody_sound.js
@@ -1,19 +1,26 @@
 $(function () {
-	// ALL_NOTES.forEach(function (note) {
-	// 	$("#audio-els").append("<audio src='note_mp3s/" + note + ".mp3' data-note='" + note + "'>");
-	// });
-
 	window.playMelodySound = function () {
-		// debugger;
 		$("#audio-els").empty();
 		var i = 0;
 		var wholeNoteLength = 4000; // TODO get this from form
-		playNextNote();
+
+		// metronome
+		$("#audio-els").append("<audio src='note_mp3s/tick.mp3'></audio>");
+		var tick = $("#audio-els :last-child")[0];
+		tick.play();
+		var metronomeID = window.setInterval(function () {
+			tick.play();
+		}, wholeNoteLength / 4);
+
+		// four ticks before notes then play notes
+		setTimeout(playNextNote, wholeNoteLength);
+
 		function playNextNote() {
 			if (i === melody.notes.length) {
+				clearInterval(metronomeID);
 				return;
 			} else {
-				$("#audio-els").append("<audio src='note_mp3s/" + melody.notes[i] + ".mp3' data-note='" + melody.notes[i] + "'>");
+				$("#audio-els").append("<audio src='note_mp3s/" + melody.notes[i] + ".mp3' data-note='" + melody.notes[i] + "'></audio>");
 				var curNote = $("#audio-els :last-child")[0];
 				curNote.play();
 				setTimeout(playNextNote, melody.durations[i] * wholeNoteLength);
