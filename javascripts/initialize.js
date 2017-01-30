34d64cecfe862ec35dbc81e2fa6607bf2acb2ba5
Philip Larie <plarie@lendingclub.com>
2017-01-29 21:02:37 -0800

    Add alert for mobile

--- javascripts/initialize.js
+++ javascripts/initialize.js
@@ -4,4 +4,9 @@ $(function () {
   SightReading.curMelody = new SightReading.Melody();
   SightReading.drawMelody(SightReading.curMelody);
   $('#audio-click').append('<audio src="note_mp3s/tick.mp3"></audio>');
+
+  // http://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device-in-jquery
+  if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    alert('If you are viewing this on a mobile device, please instead view on a desktop with a proper keyboard.');
+  }
 });
